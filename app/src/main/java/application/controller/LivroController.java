dfd48798a9441dad8e9d3e41e7db3096b0ef4ae9
marcoweb <marcocarvalho.web@gmail.com>
2024-06-07 11:05:19 +0000

    Insert Livro

--- app/src/main/java/application/controller/LivroController.java
+++ app/src/main/java/application/controller/LivroController.java
@@ -1,10 +1,16 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import application.model.Genero;
+import application.model.Livro;
 import application.repository.GeneroRepository;
 import application.repository.LivroRepository;
 
@@ -26,5 +32,21 @@ public class LivroController {
     public String insert(Model ui) {
         ui.addAttribute("generos", generoRepo.findAll());
         return "/livros/insert";
+    }
+
+    @RequestMapping(value = "/insert", method = RequestMethod.POST)
+    public String insert(
+        @RequestParam("titulo") String titulo,
+        @RequestParam("genero") long generoId
+    ) {
+        Optional<Genero> resultGenero = generoRepo.findById(generoId);
+        if(resultGenero.isPresent()) {
+            Livro livro = new Livro();
+            livro.setTitulo(titulo);
+            livro.setGenero(resultGenero.get());
+
+            livroRepo.save(livro);
+        }
+        return "redirect:/livros/list";
     }
 }
